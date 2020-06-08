b2af099471494c23e8d0a4031d53351468fad6cd
vairamuthu shanmugavel <vairamuthu.sv@zohocorp.com>
2020-06-08 20:36:49 +0530

    June 8 coding problem

--- word to vector/index.js
+++ word to vector/index.js
@@ -1,11 +1,26 @@
-var dataset;
+var dataMap = {};
 async function processData() {
- let data =await fetch("dataset.json");
- data = await data.json()
- 
- 
-
+  let data = await fetch("dataset.json");
+  data = await data.json();
+  for (let color of data.colors) {
+    dataMap[color.color] = { rgba: convertTextToRGB(color.color), ...color };
+  }
 }
+
+function calcEculideanDist(colorA, colorB) {
+  return Math.sqrt(
+    Math.pow((colorA.r - colorB.r), 2) +
+      Math.pow((colorA.g - colorB.g), 2) +
+      Math.pow((colorA.b - colorB.b), 2)
+  );
+}
+
+/**
+ *
+ * @param {string} colorName A valid colorName
+ * @return {Object} rgba value
+ *
+ */
 function convertTextToRGB(colorName) {
   ctx.beginPath();
   ctx.fillStyle = colorName;
@@ -15,7 +30,9 @@ function convertTextToRGB(colorName) {
   return { r: imagedata[0], g: imagedata[1], b: imagedata[1], a: imagedata[4] };
 }
 
-//taking event from the form panel
+/**
+ * @description  Listening to form submit event.
+ */
 let form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
